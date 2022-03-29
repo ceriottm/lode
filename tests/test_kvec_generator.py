fb4c7bad0fb878817897a9b1fd4c79c8e7e24587
Philip Loche <ploche@physik.fu-berlin.de>
2022-03-29 18:08:17 +0200

    Remove dublicated code from kvec_test

--- tests/test_kvec_generator.py
+++ tests/test_kvec_generator.py
@@ -6,8 +6,12 @@ Created on Wed Jan 19 13:32:41 2022
 """
 
 import numpy as np
+import pytest
+import scipy
 
 from pylode.lib.kvec_generator import KvectorGenerator
+
+np.random.seed(12419)
 
 
 class TestKvecgen:
@@ -27,9 +31,16 @@ class TestKvecgen:
     ]
     num_vectors_correct = [3, 3, 9, 9, 13, 13, 16, 16]
 
-    def test_kvecgen_cubic(self):
-        """Use simple cubic cell for which the solution is known exactly."""
-        cell = np.eye(3)
+    cell_cubic = np.eye(3)
+    cell_triclinic = np.array([[1, 0, 0], [5, 1, 0], [3, 4, 1]])
+
+    Q = scipy.linalg.qr(np.random.normal(0, 1, (3, 3)))[0]
+    cell_rotated = np.array([[1, 0, 0], [5, 1, 0], [3, 4, 1]]) @ Q
+
+    @pytest.mark.parametrize("cell",
+                             [cell_cubic, cell_triclinic, cell_rotated])
+    def test_kvecgen(self, cell):
+        """Use simple cell for which the solution is known exactly."""
 
         for ik, kcut in enumerate(self.cutoffs):
             # Generate k vectors
@@ -45,46 +56,4 @@ class TestKvecgen:
             # Check that the obtained normes are indeed the norms of the
             # corresponding k-vectors and that they lie in the cutoff ball
             assert (kvecnorms == np.linalg.norm(kvectors, axis=1)).all()
-            assert (kvecnorms < kcut).all()
-
-    def test_kvecgen_triclinic(self):
-        """Use triclinic input vectors (describing the same cell as above)."""
-        cell = np.array([[1, 0, 0], [5, 1, 0], [3, 4, 1]])
-        for ik, kcut in enumerate(self.cutoffs):
-            # Generate k vectors
-            kvecgen = KvectorGenerator(cell, kcut, is_reciprocal_cell=True)
-            kvecgen.compute()
-            kvectors = kvecgen.kvectors
-            kvecnorms = kvecgen.kvector_norms
-
-            # Check whether number of obtained vectors agrees with exact result
-            assert len(kvectors) == kvecgen.kvector_number
-            assert kvecgen.kvector_number == self.num_vectors_correct[ik]
-
-            # Check that the obtained normes are indeed the norms of the
-            # corresponding k-vectors and that they lie in the cutoff ball
-            assert (kvecnorms == np.linalg.norm(kvectors, axis=1)).all()
-            assert (kvecnorms < kcut).all()
-
-    def test_kvecgen_rotated(self):
-        """Use rotated cell"""
-        # Generate random rotation matrix
-        np.random.seed(12419)
-        import scipy
-        Q, R = scipy.linalg.qr(np.random.normal(0, 1, (3, 3)))
-        assert np.linalg.norm(Q.T @ Q - np.eye(3)) < 1e-13
-        cell = np.array([[1, 0, 0], [5, 1, 0], [3, 4, 1]]) @ Q
-        for ik, kcut in enumerate(self.cutoffs):
-            # Generate k vectors
-            kvecgen = KvectorGenerator(cell, kcut, is_reciprocal_cell=True)
-            kvecgen.compute()
-            kvectors = kvecgen.kvectors
-            kvecnorms = kvecgen.kvector_norms
-
-            # Check whether number of obtained vectors agrees with exact result
-            assert len(kvectors) == kvecgen.kvector_number
-            assert kvecgen.kvector_number == self.num_vectors_correct[ik]
-
-            # Check that the obtained normes are indeed the norms of the
-            # corresponding k-vectors and that they lie in the cutoff ball
             assert (kvecnorms < kcut).all()
