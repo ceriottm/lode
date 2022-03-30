12da88fb2b9536f4d579dd2d56496a1fe8560318
Philip Loche <ploche@physik.fu-berlin.de>
2022-03-30 10:49:41 +0200

    Added "-dev" to version

--- src/pylode/__init__.py
+++ src/pylode/__init__.py
@@ -7,5 +7,5 @@ from pylode.lib.projection_coeffs import DensityProjectionCalculator
 
 __all__ = ["DensityProjectionCalculator"]
 
-__version__ = '0.0.0'
+__version__ = '0.0.0-dev'
 __author__ = "COSMONAUTS"
